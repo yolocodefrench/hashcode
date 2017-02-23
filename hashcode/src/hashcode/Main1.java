5731cdfc5c26a6a0c0cd8fa1d299afd8e17fc2cc
danyCorbineau <dany.corbineau@ynov.com>
2017-02-23 18:29:53 +0100

    Update Main1.java

--- hashcode/src/hashcode/Main1.java
+++ hashcode/src/hashcode/Main1.java
@@ -1,10 +1,16 @@
 package hashcode;
 
+import java.util.Scanner;
+
 public class Main1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		
+		//sc.nextLine();
+		//sc.next();
+		
+		sc.close();
 	}
 
 }
